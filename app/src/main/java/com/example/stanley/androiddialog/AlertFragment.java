5d0e4f8c688823c25883e53d01c72d1f90135a37
adigwestanley <Stanley@STANLEYs-MacBook-Air.local>
2015-07-30 11:26:43 -0400

    commit
    New Proj

---
+++ app/src/main/java/com/example/stanley/androiddialog/AlertFragment.java
@@ -0,0 +1,45 @@
+package com.example.stanley.androiddialog;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by Stanley on 2015-07-28.
+ */
+public class AlertFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle saveInstanceState) {
+        Dialog dialog = new Dialog(getActivity());
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+
+        dialog.setContentView(R.layout.dialog_custom);
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        // POSITIVE BUTTON
+        dialog.findViewById(R.id.positive_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        //NEGATIVE BUTTON
+        dialog.findViewById(R.id.close_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        return dialog;
+
+    }
+}
