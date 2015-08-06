c73973e70a017658fb5c7348a6468f8da31133ef
adigwestanley <Stanley@STANLEYs-MacBook-Air.local>
2015-08-06 15:12:43 -0400

    commit

--- app/src/main/java/com/example/stanley/androiddialog/AlertFragment.java
+++ app/src/main/java/com/example/stanley/androiddialog/AlertFragment.java
@@ -2,6 +2,7 @@ package com.example.stanley.androiddialog;
 
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -10,7 +11,7 @@ import android.view.Window;
 import android.view.WindowManager;
 
 /**
- * Created by Stanley on 2015-07-28.
+ * Created by Stanley on 2015-07-30.
  */
 public class AlertFragment extends DialogFragment {
 
@@ -19,16 +20,19 @@ public class AlertFragment extends DialogFragment {
         Dialog dialog = new Dialog(getActivity());
         dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
-
         dialog.setContentView(R.layout.dialog_custom);
-
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
         // POSITIVE BUTTON
         dialog.findViewById(R.id.positive_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+              Intent intent = new Intent(getActivity(), NewActivity.class);
+                startActivity(intent);
+//                NewActivity myFragment = new NewActivity();
+//                myFragment._showDialog();
                 dismiss();
+
             }
         });
 
@@ -43,3 +47,5 @@ public class AlertFragment extends DialogFragment {
 
     }
 }
+
+
